d4c627899e0c8bc934d8f465c91665297d6a3ef3
MahaCreative <gunturmadjid.3@gmail.com>
2024-06-26 03:25:49 -0700

    update

--- resources/js/Pages/Fasilitas/Page/Index.jsx
+++ resources/js/Pages/Fasilitas/Page/Index.jsx
@@ -300,23 +300,19 @@ export default function Index(props) {
                                                     </button>
                                                 </div>
                                             )}
-                                            {role == "pengunjung" &&
-                                                role == null && (
-                                                    <div>
-                                                        <button
-                                                            onClick={() =>
-                                                                lihatHandler(
-                                                                    item
-                                                                )
-                                                            }
-                                                            className="bg-blue-500 hover:bg-blue-800 transition-all ease-in-out duration-300 text-white py-1 px-2 leading-none tracking-tighter"
-                                                        >
-                                                            <Tooltip title="Lihat Detail Paket">
-                                                                <Visibility />
-                                                            </Tooltip>
-                                                        </button>
-                                                    </div>
-                                                )}
+
+                                            <div>
+                                                <button
+                                                    onClick={() =>
+                                                        lihatHandler(item)
+                                                    }
+                                                    className="bg-blue-500 hover:bg-blue-800 transition-all ease-in-out duration-300 text-white py-1 px-2 leading-none tracking-tighter"
+                                                >
+                                                    <Tooltip title="Lihat Detail Paket">
+                                                        <Visibility />
+                                                    </Tooltip>
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </ScrollAnimation>
